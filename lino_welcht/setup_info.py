4b5b7d2189bae9db69f3ac8ab925b347009e3cc0
Luc Saffre <luc.saffre@gmail.com>
2019-03-27 05:59:40 +0200

    http://luc.lino-framework.org/blog/2019/0326.html

--- lino_welcht/setup_info.py
+++ lino_welcht/setup_info.py
@@ -39,12 +39,14 @@ SETUP_INFO = dict(
 `Lino Welfare <http://welfare.lino-framework.org>`__
 application developed and maintained for the PCSW of Châtelet in Belgium. 
 
+- The central project homepage is published http://welcht.lino-framework.org
+
 - For *introductions* and *commercial information* 
-  please see `www.saffre-rumma.net
+  see `www.saffre-rumma.net
   <http://www.saffre-rumma.net/welfare/>`__.
 
-- The `central project homepage <http://welcht.lino-framework.org>`__ 
-  contains change notes, technical specifications and test suites.
+- Technical specifications and test suites are in 
+  `Lino Welfare <http://welfare.lino-framework.org>`__.
 
 - The `French project homepage <http://fr.welfarelino-framework.org>`__ 
   contains release notes and end-user docs.
